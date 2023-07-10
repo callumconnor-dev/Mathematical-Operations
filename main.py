25b9320d9df59ad23008a7cb4f1f21fb0edc4a78
callumconnor-dev <132172088+callumconnor-dev@users.noreply.github.com>
2023-07-10 23:23:11 +0100

    Added Modulo

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ print("Addition: " + str(int(twoDigitNumber[0]) + int(twoDigitNumber[1])))
 print("Subtraction: " + str(int(twoDigitNumber[0]) - int(twoDigitNumber[1])))
 print("Multiplication: " + str(int(twoDigitNumber[0]) * int(twoDigitNumber[1])))
 print("Division: " + str(int(twoDigitNumber[0]) / int(twoDigitNumber[1])))
+print("Modulo: " + str(int(twoDigitNumber[0]) % int(twoDigitNumber[1]))) # Modulo gives the remainder and is called using the % sign
 print(str(int(twoDigitNumber[0])) +" to the power of " + str(int(twoDigitNumber[1])) + ": " + str(int(twoDigitNumber[0]) ** int(twoDigitNumber[1])))
 print("Floor Division: " + str(int(twoDigitNumber[0]) // int(twoDigitNumber[1]))) # Normal division but rounds to 0-point
 print("Round to 2-point: " + str(round(int(twoDigitNumber[0]) / int(twoDigitNumber[1]), 2))) # Rounds to two decimal points
+
 
 # Remember BIDMAS - Brackets () -> Indices ** -> Division and Multiplication are Equal / * -> Addition and Subtraction are also equal + -
 # In cases where operations are of equal priority it goes Left to Right
